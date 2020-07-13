02fe6785fa4a6daff5e6b2e21996182dfb6fe1fe
Lukasz Rej <rejlukasz@gmail.com>
2020-07-13 16:33:35 +0200

    Updates

---
+++ precache-manifest.95403b16843e2a8d59bb440b3411b3a7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f32464f7d07314c4ba80a51d487ec90a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "94c1cf18874da5fba529",
+    "url": "./static/js/2.484763a3.chunk.js"
+  },
+  {
+    "revision": "eac4cf64bf573541fd6dba29c78393a0",
+    "url": "./static/js/2.484763a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "497079962531f990b176",
+    "url": "./static/js/main.40572797.chunk.js"
+  },
+  {
+    "revision": "fb11a5cf54210ecff721",
+    "url": "./static/js/runtime-main.840ff753.js"
+  }
+]);
