ec4796d8bb5309d070bed7541f6425fc06ceb1ab
Lukasz Rej <rejlukasz@gmail.com>
2020-07-19 11:00:41 +0200

    Updates

---
+++ precache-manifest.276ef843fb1ec9ef3f58f66069b9d73c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3ede0aaa47ff01dab3c12992ae243eb",
+    "url": "./index.html"
+  },
+  {
+    "revision": "4d34df96bf2d25c58ae5",
+    "url": "./static/js/2.ad818fcb.chunk.js"
+  },
+  {
+    "revision": "c7a7e79b10ab645cab64be742e1dd9e5",
+    "url": "./static/js/2.ad818fcb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc19708cc693558febca",
+    "url": "./static/js/main.9c34b007.chunk.js"
+  },
+  {
+    "revision": "fb11a5cf54210ecff721",
+    "url": "./static/js/runtime-main.840ff753.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.608aefbed694c3894de7393239e41c82.js"
+  "./precache-manifest.276ef843fb1ec9ef3f58f66069b9d73c.js"
 );
 
 self.addEventListener('message', (event) => {
